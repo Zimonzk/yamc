e111918190729a5c3b629984504bd213ea57c44b
Zimonzk <simon.artner@gmail.com>
2018-09-17 22:44:41 +0200

    fix problems in code due to folder restructuring

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # YAMC - Yet Another Minecraft Clone
 Really, that's it.
+Also sorry for any german appearing in the project. ;)
--- src/rendering.c
+++ src/rendering.c
@@ -24,108 +24,108 @@ extern long player_chunk_offset[2];
 
 // An array of 3 vectors which represents 3 vertices
 static const GLfloat g_vertex_buffer_data[] = {
-    -1, -1, 1, /* cube front lower */
-    1, -1, 1,
-    1, 1, 1,
+	-1, -1, 1, /* cube front lower */
+	1, -1, 1,
+	1, 1, 1,
 
-    -1, -1, 1, /* cube front upper */
-    1, 1, 1,
-    -1, 1, 1,
+	-1, -1, 1, /* cube front upper */
+	1, 1, 1,
+	-1, 1, 1,
 
-    1, -1, 1, /* cube right lower */
-    1, -1, -1,
-    1, 1, -1,
+	1, -1, 1, /* cube right lower */
+	1, -1, -1,
+	1, 1, -1,
 
-    1, -1, 1, /* cube right upper */
-    1, 1, -1,
-    1, 1, 1,
+	1, -1, 1, /* cube right upper */
+	1, 1, -1,
+	1, 1, 1,
 
-    1, -1, -1, /* cube back lower */
-    -1, -1, -1,
-    -1, 1, -1,
+	1, -1, -1, /* cube back lower */
+	-1, -1, -1,
+	-1, 1, -1,
 
-    1, -1, -1, /* cube back upper */
-    -1, 1, -1,
-    1, 1, -1,
+	1, -1, -1, /* cube back upper */
+	-1, 1, -1,
+	1, 1, -1,
 
-    -1, -1, -1, /* cube left lower */
-    -1, -1, 1,
-    -1, 1, 1,
+	-1, -1, -1, /* cube left lower */
+	-1, -1, 1,
+	-1, 1, 1,
 
-    -1, -1, -1, /* cube left upper */
-    -1, 1, 1,
-    -1, 1, -1,
+	-1, -1, -1, /* cube left upper */
+	-1, 1, 1,
+	-1, 1, -1,
 
-    -1, 1, 1, /* cube top front */
-    1, 1, 1,
-    1, 1, -1,
+	-1, 1, 1, /* cube top front */
+	1, 1, 1,
+	1, 1, -1,
 
-    -1, 1, 1, /* cube top back */
-    1, 1, -1,
-    -1, 1, -1,
+	-1, 1, 1, /* cube top back */
+	1, 1, -1,
+	-1, 1, -1,
 
-    1, -1, 1, /* cube bottom front */
-    -1, -1, 1,
-    -1, -1, -1,
+	1, -1, 1, /* cube bottom front */
+	-1, -1, 1,
+	-1, -1, -1,
 
-    1, -1, 1, /* cube bottom back */
-    -1, -1, -1,
-    1, -1, -1,
+	1, -1, 1, /* cube bottom back */
+	-1, -1, -1,
+	1, -1, -1,
 };
 
 static const GLfloat g_uv_buffer_data[] = {
-    0, 0,
-    1, 0,
-    1, 1,
+	0, 0,
+	1, 0,
+	1, 1,
 
-    0, 0,
-    1, 1,
-    0, 1,
-
-
-    0, 0,
-    1, 0,
-    1, 1,
-
-    0, 0,
-    1, 1,
-    0, 1,
+	0, 0,
+	1, 1,
+	0, 1,
 
 
-    0, 0,
-    1, 0,
-    1, 1,
+	0, 0,
+	1, 0,
+	1, 1,
 
-    0, 0,
-    1, 1,
-    0, 1,
-
-
-    0, 0,
-    1, 0,
-    1, 1,
-
-    0, 0,
-    1, 1,
-    0, 1,
+	0, 0,
+	1, 1,
+	0, 1,
 
 
-    0, 0,
-    1, 0,
-    1, 1,
+	0, 0,
+	1, 0,
+	1, 1,
 
-    0, 0,
-    1, 1,
-    0, 1,
+	0, 0,
+	1, 1,
+	0, 1,
 
 
-    0, 0,
-    1, 0,
-    1, 1,
+	0, 0,
+	1, 0,
+	1, 1,
 
-    0, 0,
-    1, 1,
-    0, 1
+	0, 0,
+	1, 1,
+	0, 1,
+
+
+	0, 0,
+	1, 0,
+	1, 1,
+
+	0, 0,
+	1, 1,
+	0, 1,
+
+
+	0, 0,
+	1, 0,
+	1, 1,
+
+	0, 0,
+	1, 1,
+	0, 1
 };
 
 static GLfloat model_scale = 0.50;
@@ -162,36 +162,29 @@ extern chunk* neig[4];
 
 void render_init()
 {
-    /* init glew */
-    glewExperimental = GL_TRUE;
-    glewInit();
+	/* init glew */
+	glewExperimental = GL_TRUE;
+	glewInit();
 
-    /* This makes our buffer swap syncronized with the monitor's vertical refresh */
-    SDL_GL_SetSwapInterval(0);
-
-
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
-    glGenBuffers(1, &vertexbuffer);
-    // The following commands will talk about our 'vertexbuffer' buffer
-    //glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // Give our vertices to OpenGL.
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-    glGenBuffers(1, &texibuffer);
-    numTriangles = generate_mescha(&world[0][0], neig, vertexbuffer, texibuffer);
+	/* This makes our buffer swap syncronized with the monitor's vertical refresh */
+	SDL_GL_SetSwapInterval(0);
 
 
-	//glGenBuffers(1, &uvbuffer);
-	//glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
 
-    // Create one OpenGL texture
+
+	glGenBuffers(1, &texibuffer);
+	numTriangles = generate_mescha(&world[0][0], neig, vertexbuffer, texibuffer);
+
+
+	// Create one OpenGL texture
 	GLuint textureID;
 	glGenTextures(1, &textureID);
 
-    // Create one OpenGL texture
-    /*texture = */loadBMP_custom("side.bmp", textureID, 1);
-    /*texture2 = */loadBMP_custom("top.bmp", textureID, 2);
+	// Create one OpenGL texture
+	/*texture = */loadBMP_custom("textures/blocks/side.bmp", textureID, 1);
+	/*texture2 = */loadBMP_custom("textures/blocks/top.bmp", textureID, 2);
 
 
 	// Enable depth test
@@ -201,105 +194,105 @@ void render_init()
 
 	glEnable(GL_CULL_FACE);
 
-    /* start with a background color */
-    glClearColor( 0.6, 0.8, 1.0, 1.0 ); /* ungefähr himmelblau*/
-    glClear ( GL_COLOR_BUFFER_BIT );
+	/* start with a background color */
+	glClearColor( 0.6, 0.8, 1.0, 1.0 ); /* ungefähr himmelblau*/
+	glClear ( GL_COLOR_BUFFER_BIT );
 
-    /* create vao */
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
+	/* create vao */
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
 
-    // Bind our texture in Texture Unit 0
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D_ARRAY, textureID);
+	// Bind our texture in Texture Unit 0
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D_ARRAY, textureID);
 
-    /*glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, texture2);*/
+	/*glActiveTexture(GL_TEXTURE1);
+	  glBindTexture(GL_TEXTURE_2D, texture2);*/
 
 
-    programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
-    glUseProgram(programID);
+	programID = LoadShaders("shaders/SimpleVertexShader.vertexshader", "shaders/SimpleFragmentShader.fragmentshader");
+	glUseProgram(programID);
 
-    // Get a handle for our "myTextureSampler" uniform
-    textureSamplerID = glGetUniformLocation(programID, "my_sampler");
-    SDL_Log("sampler: %i", textureSamplerID);
+	// Get a handle for our "myTextureSampler" uniform
+	textureSamplerID = glGetUniformLocation(programID, "my_sampler");
+	SDL_Log("sampler: %i", textureSamplerID);
 
-    // Get a handle for our "MVP" uniform
-    // Only during the initialisation
-    mvpID = glGetUniformLocation(programID, "MVP");
+	// Get a handle for our "MVP" uniform
+	// Only during the initialisation
+	mvpID = glGetUniformLocation(programID, "MVP");
 
-    update_projection();
+	update_projection();
 }
 
 void update_mvp()
 {
-    float help0[4][4] = {};
-    mult_mat4_mat4(view, model, help0);
-    mult_mat4_mat4(projection, help0, mvp); /*now we should have a real mvp matrix*/
+	float help0[4][4] = {};
+	mult_mat4_mat4(view, model, help0);
+	mult_mat4_mat4(projection, help0, mvp); /*now we should have a real mvp matrix*/
 
-    // Send our transformation to the currently bound shader, in the "MVP" uniform
-    // This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
-    glUniformMatrix4fv(mvpID, 1, GL_FALSE, mvp);
+	// Send our transformation to the currently bound shader, in the "MVP" uniform
+	// This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
+	glUniformMatrix4fv(mvpID, 1, GL_FALSE, mvp);
 }
 
 void update_model()
 {
-    get_player_pos(camera_position);
-    float model_translation_mat[4][4] = {
-        {1,0,0,0},
-        {0,1,0,0},
-        {0,0,1,0},
-        {model_position[0],model_position[1],model_position[2],1}};
-    float model_scaling_matrix[4][4] = {
-        {model_scale,0,0,0},
-        {0,model_scale,0,0},
-        {0,0,model_scale,0},
-        {0,0,0,1}};
-    /*float mrq[4] = { /* model rotation quaternion *//*
-        model_rotation_axis[0] * (float) sin(model_rotation_angle / 2),
-        model_rotation_axis[1] * sin(model_rotation_angle / 2),
-        model_rotation_axis[2] * sin(model_rotation_angle / 2),
-        cos(model_rotation_angle / 2)};
-    float mehl1[4][4] = {
-        {mrq[3], -mrq[2], mrq[1], -mrq[0]},
-        {mrq[2], mrq[3], -mrq[0], -mrq[1]},
-        {-mrq[1], mrq[0], mrq[3], -mrq[2]},
-        {mrq[0], mrq[1], mrq[2], mrq[3]}};
-    float mehl2[4][4] = {
-        {mrq[3], -mrq[2], mrq[1], mrq[0]},
-        {mrq[2], mrq[3], -mrq[0], mrq[1]},
-        {-mrq[1], mrq[0], mrq[3], mrq[2]},
-        {-mrq[0], -mrq[1], -mrq[2], mrq[3]}};
-    float model_rotation_mat[4][4] = {};
-    float help0[4][4] = {};
+	get_player_pos(camera_position);
+	float model_translation_mat[4][4] = {
+		{1,0,0,0},
+		{0,1,0,0},
+		{0,0,1,0},
+		{model_position[0],model_position[1],model_position[2],1}};
+	float model_scaling_matrix[4][4] = {
+		{model_scale,0,0,0},
+		{0,model_scale,0,0},
+		{0,0,model_scale,0},
+		{0,0,0,1}};
+	/*float mrq[4] = { /* model rotation quaternion *//*
+							     model_rotation_axis[0] * (float) sin(model_rotation_angle / 2),
+							     model_rotation_axis[1] * sin(model_rotation_angle / 2),
+							     model_rotation_axis[2] * sin(model_rotation_angle / 2),
+							     cos(model_rotation_angle / 2)};
+							     float mehl1[4][4] = {
+							     {mrq[3], -mrq[2], mrq[1], -mrq[0]},
+							     {mrq[2], mrq[3], -mrq[0], -mrq[1]},
+							     {-mrq[1], mrq[0], mrq[3], -mrq[2]},
+							     {mrq[0], mrq[1], mrq[2], mrq[3]}};
+							     float mehl2[4][4] = {
+							     {mrq[3], -mrq[2], mrq[1], mrq[0]},
+							     {mrq[2], mrq[3], -mrq[0], mrq[1]},
+							     {-mrq[1], mrq[0], mrq[3], mrq[2]},
+							     {-mrq[0], -mrq[1], -mrq[2], mrq[3]}};
+							     float model_rotation_mat[4][4] = {};
+							     float help0[4][4] = {};
 
-    mult_mat4_mat4(mehl1, mehl2, model_rotation_mat);
+							     mult_mat4_mat4(mehl1, mehl2, model_rotation_mat);
 
-    mult_mat4_mat4(model_rotation_mat, model_scaling_matrix, help0);
+							     mult_mat4_mat4(model_rotation_mat, model_scaling_matrix, help0);
 
-    mult_mat4_mat4(model_translation_mat, help0, model); /*now we should have a model matrix*/
-    mult_mat4_mat4(model_translation_mat, model_scaling_matrix, model);
+							     mult_mat4_mat4(model_translation_mat, help0, model); /*now we should have a model matrix*/
+	mult_mat4_mat4(model_translation_mat, model_scaling_matrix, model);
 }
 
 void update_view()
 {
-    float help0[4][4] = {};
-    get_player_ori(camera_center);
-    get_player_right(help0);
-    vec3_cross(help0, camera_center, up_vector);
+	float help0[4][4] = {};
+	get_player_ori(camera_center);
+	get_player_right(help0);
+	vec3_cross(help0, camera_center, up_vector);
 
-    camera_position[0] = 0;
-    camera_position[1] = 0;
-    camera_position[2] = 0;
+	camera_position[0] = 0;
+	camera_position[1] = 0;
+	camera_position[2] = 0;
 
-    vec3_add(camera_center, camera_position, camera_center);
+	vec3_add(camera_center, camera_position, camera_center);
 
-    lookAtRH(camera_position, camera_center, up_vector, view); /*now we should have a view matrix*/
+	lookAtRH(camera_position, camera_center, up_vector, view); /*now we should have a view matrix*/
 }
 
 void update_projection()
 {
-    perspectiveRH((fovDeg * (float)M_PI)/180.0, 4.0/3.0, 0.1, 100.0, projection); /*now we should have a projection matrix*/
+	perspectiveRH((fovDeg * (float)M_PI)/180.0, 4.0/3.0, 0.1, 100.0, projection); /*now we should have a projection matrix*/
 }
 
 void render()
@@ -309,91 +302,91 @@ void render()
 
 void render_chunk(chunk* cchunk)
 {
-    //TODO
-    int x = 0, y = 0, z = 0;
-    int i;
-    float playerpos[3];
+	//TODO
+	int x = 0, y = 0, z = 0;
+	int i;
+	float playerpos[3];
 
-    get_player_pos(playerpos);
+	get_player_pos(playerpos);
 
-    update_view();
+	update_view();
 
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glVertexAttribPointer(
-           0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-           3,                  // size
-           GL_FLOAT,           // type
-           GL_FALSE,           // normalized?
-           0,                  // stride
-           (void*)0            // array buffer offset
-    );
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+			);
 
-    // 2nd attribute buffer : UVs
-    //glEnableVertexAttribArray(1);
-    //glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-    /*glVertexAttribPointer(
-			1,                 // attribute. No particular reason for 1, but must match the layout in the shader.
-			2,                 // size : U+V => 2
-			GL_FLOAT,          // type
-			GL_FALSE,          // normalized?
-			0,                 // stride
-            (void*)0           // array buffer offset
-    );*/
+	// 2nd attribute buffer : UVs
+	//glEnableVertexAttribArray(1);
+	//glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+	/*glVertexAttribPointer(
+	  1,                 // attribute. No particular reason for 1, but must match the layout in the shader.
+	  2,                 // size : U+V => 2
+	  GL_FLOAT,          // type
+	  GL_FALSE,          // normalized?
+	  0,                 // stride
+	  (void*)0           // array buffer offset
+	  );*/
 
-    glEnableVertexAttribArray(2);
-    glBindBuffer(GL_ARRAY_BUFFER, texibuffer);
-    glVertexAttribPointer(
-           2,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-           1,                  // size
-           GL_UNSIGNED_INT,    // type
-           GL_FALSE,           // normalized?
-           0,                  // stride
-           (void*)0            // array buffer offset
-    );
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, texibuffer);
+	glVertexAttribPointer(
+			2,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			1,                  // size
+			GL_UNSIGNED_INT,    // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+			);
 
-    // Clear the screen
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Clear the screen
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /*for(x=0; x<1; x++) {
-        for(z=0; z<1; z++) {
-            for(y=0; y<1; y++) {
-                model_position[0] = x - playerpos[0] + 64 * (cchunk->offset_X - player_chunk_offset[0]);
-                model_position[1] = y - playerpos[1];
-                model_position[2] = z - playerpos[2] + 64 * (cchunk->offset_Z - player_chunk_offset[1]);
+	/*for(x=0; x<1; x++) {
+	  for(z=0; z<1; z++) {
+	  for(y=0; y<1; y++) {
+	  model_position[0] = x - playerpos[0] + 64 * (cchunk->offset_X - player_chunk_offset[0]);
+	  model_position[1] = y - playerpos[1];
+	  model_position[2] = z - playerpos[2] + 64 * (cchunk->offset_Z - player_chunk_offset[1]);
 
-                update_model();
-                update_mvp();
+	  update_model();
+	  update_mvp();
 
-                // Set our "myTextureSampler" sampler to user Texture Unit 0
-                glUniform1i(textureID, 0);
-                // Draw the triangle !
-                glDrawArrays(GL_TRIANGLES, 0, NUMVERT-12); // Starting from vertex 0; 3 vertices total -> 1 triangle
+	// Set our "myTextureSampler" sampler to user Texture Unit 0
+	glUniform1i(textureID, 0);
+	// Draw the triangle !
+	glDrawArrays(GL_TRIANGLES, 0, NUMVERT-12); // Starting from vertex 0; 3 vertices total -> 1 triangle
 
-                glUniform1i(textureID, 1);
-                glDrawArrays(GL_TRIANGLES, NUMVERT-12, 12);
-            }
-        }
-    }*/
-    model_position[0] = x - playerpos[0] + 64 * (cchunk->offset_X - player_chunk_offset[0]);
-    model_position[1] = y - playerpos[1];
-    model_position[2] = z - playerpos[2] + 64 * (cchunk->offset_Z - player_chunk_offset[1]);
+	glUniform1i(textureID, 1);
+	glDrawArrays(GL_TRIANGLES, NUMVERT-12, 12);
+	}
+	}
+	}*/
+	model_position[0] = x - playerpos[0] + 64 * (cchunk->offset_X - player_chunk_offset[0]);
+	model_position[1] = y - playerpos[1];
+	model_position[2] = z - playerpos[2] + 64 * (cchunk->offset_Z - player_chunk_offset[1]);
 
-    update_model();
-    update_mvp();
+	update_model();
+	update_mvp();
 
-    /*too slow
-    for(i=0; i < numTriangles; i+=4) {
-        glUniform1i(textureID, 0);
-        glDrawArrays(GL_TRIANGLES, 3*i, 6);
-        glUniform1i(textureID, 1);
-        glDrawArrays(GL_TRIANGLES, 3*(i+2), 6);
-    }*/
-    //single texture
-    glUniform1i(textureSamplerID, 0);
-    glDrawArrays(GL_TRIANGLES, 0, 3*numTriangles);
+	/*too slow
+	  for(i=0; i < numTriangles; i+=4) {
+	  glUniform1i(textureID, 0);
+	  glDrawArrays(GL_TRIANGLES, 3*i, 6);
+	  glUniform1i(textureID, 1);
+	  glDrawArrays(GL_TRIANGLES, 3*(i+2), 6);
+	  }*/
+	//single texture
+	glUniform1i(textureSamplerID, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 3*numTriangles);
 
-    glDisableVertexAttribArray(0);
-    //glDisableVertexAttribArray(1);
-    glDisableVertexAttribArray(2);
+	glDisableVertexAttribArray(0);
+	//glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(2);
 }
